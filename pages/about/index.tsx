a02d6bdb256a113623ae5f8e8d62edc91948c391
SamScott3000 <samoscarscott@gmail.com>
2022-06-09 21:17:09 +0100

    Wip text in about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,22 +8,15 @@ const About: NextPage = () => {
         <Layout>
         <div className="fixed hover:pointer z-10"><Link href='/'>Home</Link></div>
           <div className="mt-20 grid grid-cols-4 gap-12">
-            <h1>About</h1>
-            <p className="col-span-2 ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <div className="col-span-1 bg-red-500"></div>
+            
           </div>
         </Layout>
       
         <Layout>
+          
           <h1>Contact</h1>
+          <p className="mt-4">samoscarscott@gmail.com</p>
+          <p className="text-center mt-32 bg-black text-white">You&apos;re currently viewing a work in progress.</p>
         </Layout>
 
     </>
