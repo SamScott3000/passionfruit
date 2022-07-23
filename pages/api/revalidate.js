1ddfbb688af94db3244a36ee5057828001779552
SamScott3000 <samoscarscott@gmail.com>
2022-07-23 01:36:19 +0100

    Give a go

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,5 +1,6 @@
 import * as prismic from "@prismicio/client";
 import * as prismicH from "@prismicio/helpers";
+import { url } from "inspector";
 
 // Import your app's Link Resolver (if your app uses one)
 import { linkResolver } from "../../prismicio";
@@ -13,10 +14,7 @@ import { linkResolver } from "../../prismicio";
 export default async function handler(req, res) {
   if (req.body.type === "api-update" && req.body.documents.length > 0) {
     // Check for secret to confirm this is a valid request
-    if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
-      return res.status(401).json({ message: "Invalid token" });
-    }
-  
+
     // If you have a `createClient()` function defined elsewhere in
     // your app, use that instead
     const client = prismic.createClient("passion-fruit");
@@ -24,11 +22,10 @@ export default async function handler(req, res) {
     // Get a list of URLs for any new, updated, or deleted documents
     const documents = await client.getAllByIDs(req.body.documents);
     const urls = documents.map((doc) => prismicH.asLink(doc, linkResolver));
-
     try {
       // Revalidate the URLs for those documents
-      await Promise.all(urls.map(async (url) => await res.revalidate(url)));
-
+      res.revalidate(urls)
+      // await Promise.all(urls.map(async (url) => await res.revalidate(url)));
       return res.json({ revalidated: true });
     } catch (err) {
       // If there was an error, Next.js will continue to show
